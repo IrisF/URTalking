0c5e92062d4979271e8fce2d8f85a9a1f6913b1d
Matthias Bräuer <matthias.braeuer@arcor.de>
2015-09-11 20:50:33 +0200

    fixed merge conflict in values controller

--- UR_Talking/UR_Talking/ValuesController.cs
+++ UR_Talking/UR_Talking/ValuesController.cs
@@ -15,46 +15,24 @@ namespace UR_Talking
     public class ValuesController : ApiController
     {
         private AnswerDAO answerDAO;
-<<<<<<< HEAD
-
-        public ValuesController(AnswerDAO answerDAO)
-        {
-            this.answerDAO = answerDAO;
-        }
-
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-=======
         private NLP nlp;
 
         public ValuesController(AnswerDAO answerDAO, NLP nlp)
         {
             this.answerDAO = answerDAO;
             this.nlp = nlp;
->>>>>>> origin/SharpNLP
         }
 
         // POST api/<controller>
         public string Post([FromBody]string value)
         {
-<<<<<<< HEAD
             String request = StemmerAndTokenizer.stemAndTokenize(value);
-            String answer = this.answerDAO.GetAnswer(request.Split());
-=======
-            List<SearchObject> searchObjects = nlp.GetAnswerTypList(value);
+
+            List<SearchObject> searchObjects = nlp.GetAnswerTypList(request);
 
             string answer = this.answerDAO.GetAnswer(searchObjects);
          
->>>>>>> origin/SharpNLP
-            return answer;
+            return value;
         }
     }
 }
